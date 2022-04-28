71f3c66f742f7edaa3639d1a7f49e3c87c82f518
Claas Augner <caugner@mozilla.com>
2022-04-28 19:55:20 +0200

    fix(esm): remove unused import

--- build/flaws/index.js
+++ build/flaws/index.js
@@ -12,7 +12,6 @@ import { getBrokenLinksFlaws } from "./broken-links.js";
 import { getHeadingLinksFlaws } from "./heading-links.js";
 import { getPreTagFlaws } from "./pre-tags.js";
 import { injectSectionFlaws } from "./sections.js";
-import { getUnsafeHTMLFlaws } from "./unsafe-html";
 import { getAndMarkupUnsafeHTMLFlaws } from "./unsafe-html.js";
 import { injectTranslationDifferences } from "./translation-differences.js";
 
